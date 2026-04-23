161385d85d942bf914215522d8430c0fc99e53d5
jay <jay@gmail.com>
2026-04-23 12:30:52 +0530

    unknown commit

--- text files/main.py
+++ text files/main.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np  
 
-import scikit-learn as sk
-import matplotlib.pyplot as plt 
+# import scikit-learn as sk
+# import matplotlib.pyplot as plt 
+import tensorflow as tf
